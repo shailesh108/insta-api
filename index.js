19cd98ce57ce7e555ec593e40544b65f1ae25ea1
shaileshprajapati-btc <shailesh.prajapati@botreetechnologies.com>
2020-12-23 10:50:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var axios = require('axios')
+var app = express()
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
+app.get('/getData', (req, res, next) => {
+  let tagId = req.query.tag
+  axios
+    .get(`https://www.instagram.com/explore/tags/${tagId}/?__a=1`)
+    .then(resp => {
+      res.json(resp.data)
+    })
+    .catch(e => res.json(e))
+})
