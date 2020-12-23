852cd172284f920a4676387efa16aa843afc8908
shaileshprajapati-btc <shailesh.prajapati@botreetechnologies.com>
2020-12-23 14:17:40 +0530

    port change

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var axios = require('axios')
 var app = express()
-app.listen(3000, () => {
+
+app.listen(8080, () => {
   console.log('Server running on port 3000')
 })
+
 app.get('/getData', (req, res, next) => {
   let tagId = req.query.tag
   axios
