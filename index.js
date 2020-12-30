79f1adf4277ed02914b298f140432c80ff45f7b1
shaileshprajapati-btc <shailesh.prajapati@botreetechnologies.com>
2020-12-30 22:22:40 +0530

    added puppeter for scraip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var axios = require('axios')
 const http = require('http')
 const path = require('path')
+const puppeteer = require('puppeteer')
 
 var app = express()
 const server = http.Server(app)
@@ -11,12 +12,24 @@ app.listen(process.env.PORT || 8080, () => {
   console.log('Server running on port 3000', process.env.PORT)
 })
 
-app.get('/getData', (req, res, next) => {
+app.get('/getData', async (req, res, next) => {
   let tagId = req.query.tag
-  axios
-    .get(`https://www.instagram.com/explore/tags/${tagId}/?__a=1`)
-    .then(resp => {
-      res.json(resp.data)
-    })
-    .catch(e => res.json(e))
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+      '--user-data-dir=/tmp/user_data/',
+      '--window-size=1200,800'
+    ]
+  })
+  const page = await browser.newPage()
+
+  await page.goto(`https://www.instagram.com/explore/tags/${tagId}/?__a=1`)
+  let items = await page.evaluate(() => {
+    let items = document.getElementsByTagName('pre')[0]
+    items = items && items.textContent
+    return JSON.parse(items || null)
+  })
+  await browser.close()
+  res.json(items)
 })
