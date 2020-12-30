92dd5fa5c951035b6face98e08c195a88f585095
shaileshprajapati-btc <shailesh.prajapati@botreetechnologies.com>
2020-12-30 22:49:15 +0530

    headless false

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(process.env.PORT || 8080, () => {
 app.get('/getData', async (req, res, next) => {
   let tagId = req.query.tag
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     args: [
       '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
       '--user-data-dir=/tmp/user_data/',
